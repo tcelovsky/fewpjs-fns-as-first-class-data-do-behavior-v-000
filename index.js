551c2a54466addd4d3bb7bee2b2ad1f3e928412b
Tatyana Celovsky <tatyana.celovsky@gmail.com>
2020-08-12 18:40:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ function greet(timeString) {
 }
 /* Write your implementation of displayMessage() */
 function displayMessage(greet) {
-  document.querySelector("greeting").addEventListener("click", handleClick);
+  document.getElementById("greeting").addEventListener("click", handleClick);
 }
